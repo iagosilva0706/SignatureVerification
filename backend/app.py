b3e0b8ebfe06d6b28693dc73ea25529d43d9c7e9
iagosilva0706 <iago.silva.0706@gmail.com>
2025-07-15 22:32:21 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -123,5 +123,5 @@ def verify_signature():
         return jsonify({"erro": f"Internal error: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
