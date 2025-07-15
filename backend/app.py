2db4ba434a2077602fe2713b1f2a8b2099304dfb
iagosilva0706 <iago.silva.0706@gmail.com>
2025-07-15 22:45:11 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -124,4 +124,4 @@ def verify_signature():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8000)
