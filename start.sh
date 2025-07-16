43e3ef975ba55c035269b8f7ae3fa37e5ed3d392
iagosilva0706 <iago.silva.0706@gmail.com>
2025-07-16 19:38:50 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Download model from cloud storage
-wget https://drive.google.com/file/d/1O6F8-dcxAe2jwjrBV6jZLqmKFw8WtaHl/view?usp=drive_link/model.h5 -O model.h5
-
-# Start FastAPI server
-uvicorn app:app --host=0.0.0.0 --port=10000
